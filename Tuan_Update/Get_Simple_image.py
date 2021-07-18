f567ca2723a862724c088f358224710e5ec1fa51
Le Quoc Tuan <lequoctuan.fwork@gmail.com>
2021-07-18 11:06:17 +0700

    Adding another version

---
+++ Tuan_Update/Get_Simple_image.py
@@ -0,0 +1,102 @@
+import base64
+import numpy as np
+import socketio
+import eventlet
+import eventlet.wsgi
+import cv2
+from PIL import Image
+from flask import Flask
+from io import BytesIO
+from sklearn.linear_model import RANSACRegressor
+import math
+import lane_detect_module
+#------------- Add library ------------#
+
+#--------------------------------------#
+#Global variable
+MAX_SPEED = 100
+MAX_ANGLE = 25
+# Tốc độ thời điểm ban đầu
+speed_limit = MAX_SPEED
+MIN_SPEED = 10
+
+#initialize our server
+sio = socketio.Server()
+#our flask (web) app
+app = Flask(__name__)
+#registering event handler for the server
+
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    if data:
+
+
+        steering_angle = 0  #Góc lái hiện tại của xe
+        speed_callback = 0           #Vận tốc hiện tại của xe
+        image = 0           #Ảnh gốc
+
+        steering_angle = float(data["steering_angle"])
+        speed_callback = float(data["speed"])
+        #Original Image
+        image = Image.open(BytesIO(base64.b64decode(data["image"])))
+        image = np.asarray(image)
+        
+        """
+        - Chương trình đưa cho bạn 3 giá trị đầu vào:
+            * steering_angle: góc lái hiện tại của xe
+            * speed: Tốc độ hiện tại của xe
+            * image: hình ảnh trả về từ xe
+        
+        - Bạn phải dựa vào 3 giá trị đầu vào này để tính toán và gửi lại góc lái và tốc độ xe cho phần mềm mô phỏng:
+            * Lệnh điều khiển: send_control(sendBack_angle, sendBack_Speed)
+            Trong đó:
+                + sendBack_angle (góc điều khiển): [-25, 25]  NOTE: ( âm là góc trái, dương là góc phải)
+                + sendBack_Speed (tốc độ điều khiển): [-150, 150] NOTE: (âm là lùi, dương là tiến)
+        """
+        sendBack_angle = 0
+        sendBack_Speed = 10
+        try:
+            #------------------------------------------  Work space  ----------------------------------------------#
+            # print(image.shape)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            cv2.imshow("image", image)
+            canny = lane_detect_module.preprocess(image, medianKsize = 3, s_offset = 10, 
+                                                  v_offset = 60, CannyThres1 = 100, CannyThres2 = 200)
+            # cv2.imshow("canny", canny)
+            # sendBack_angle = line_detect(canny)
+            cv2.waitKey(1)
+            #------------------------------------------------------------------------------------------------------#
+            # print('{} : {}'.format(sendBack_angle, sendBack_Speed))
+            send_control(sendBack_angle, sendBack_Speed)
+        except Exception as e:
+            print(e)
+    else:
+        sio.emit('manual', data={}, skip_sid=True)
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print(sid, "connect ")
+    send_control(0, 0)
+
+
+def send_control(steering_angle, throttle):
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__(),
+        },
+        skip_sid=True)
+
+
+
+
+if __name__ == '__main__':
+
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+    
---
+++ Tuan_Update/lane_detect_module.py
@@ -0,0 +1,176 @@
+from sklearn.linear_model import RANSACRegressor
+import numpy as np
+import cv2 as cv
+from sklearn.metrics import mean_squared_error
+from numpy.lib.polynomial import polyfit
+import math
+def pers_transform(img):
+    global birdview
+    img_size = (img.shape[1], img.shape[0])
+    src = np.float32([[0, 180], [160 - 416/7, 115],
+                     [160 + 416/7, 115], [320, 180]])
+
+    # cv2.imshow("img", img)
+    offset = [75, 0]
+    dst = np.float32([src[0] + offset, np.array([src[0, 0], 0]) + offset,
+                      np.array([src[3, 0], 0]) - offset, src[3] - offset])
+
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv.getPerspectiveTransform(src, dst)
+    # Warp the image using OpenCV warpPerspective()
+    birdview = cv.warpPerspective(img, M, img_size)
+    return birdview
+# Find average threshold of line
+def find_threshold(img):
+    roi = img[120:150, 140:180]
+    threshold = np.mean(roi)
+    return threshold
+
+# Data preprocessing
+def preprocess(img, medianKsize = 3, s_offset = 20, v_offset = 50, CannyThres1 = 100, CannyThres2 = 200):
+    """
+        preprocess function detect edges of the lane
+
+        :param img: raw image got from simulation
+        :param medianKsize: kernel size for Median Blur
+        :param s_offset: offset to find mean threshold for s image
+        :param v_offset: offset to find mean threshold for v image
+        :param CannyThres1: lower threshold for Canny Edge Detection
+        :param CannyThres2: higher threshold for Canny Edge Detection
+
+        :return: noise-filtered and canny-applied ROI
+    """
+    # Read image
+    global warped
+    # img = cv.imread(str(image_path), cv.IMREAD_COLOR)
+    warped = pers_transform(img)
+    # Convert in to HSV color space
+    hsv = cv.cvtColor(warped, cv.COLOR_BGR2HSV)
+    h, s, v = cv.split(hsv)
+    # Blurr image
+    s_blurred = cv.medianBlur(s, medianKsize)
+    v_blurred = cv.medianBlur(v, medianKsize)
+
+    # Convert to binary image
+    s_threshold = find_threshold(s) + s_offset
+    v_threshold = find_threshold(v) + v_offset
+
+    _, s_thresh = cv.threshold(s_blurred, s_threshold, 255, cv.THRESH_BINARY_INV)
+    _, v_thresh = cv.threshold(v_blurred, v_threshold, 255, cv.THRESH_BINARY)
+    # final = cv.bitwise_and(s_thresh, v_thresh)[100:, :]
+    final = cv.bitwise_and(s_thresh, v_thresh)
+    # canny = canny_edge_detect(final)
+    canny = cv.Canny(final, CannyThres1, CannyThres2)
+    cv.imshow("canny", canny)
+    return 0
+
+def downsample(ROI):
+    col_index = 0
+    m = ROI.shape[0]
+    n = ROI.shape[1]
+    result = np.zeros((m // 10, n), dtype=np.float)
+    for i in range(0, m, 10):
+        for j in range(0, n):
+            process = ROI[i: i + 10, j]
+            value = np.sum(process, dtype=np.float)
+            result[col_index][j] = value
+        col_index += 1
+    return result
+
+# Get consecutive array
+def ranges(nums):
+    gaps = [[s, e]
+            for s, e in zip(nums, nums[1:]) if (s + 1 < e and s + 70 < e)]
+    edges = iter(nums[:1] + sum(gaps, []) + nums[-1:])
+    return list(zip(edges, edges))
+
+# Determine xLeft, xMid, xRight
+def getmeanX(process, left_thres = 80, right_thres = 160):
+    dataLeft = []
+    dataRight = []
+    # dataMid = []
+    for i in range(process.shape[0]):
+        ROI = process[i, :]
+        xValue = np.where(ROI > 0)
+        x = ranges(list(xValue[0]))
+        try:
+            for j in x:
+                if (np.mean(j, dtype=np.int) <= left_thres):
+                    left = [np.mean(j, dtype=np.int), 5 + i * 10]
+                    dataLeft.append(left)
+                    # Tuple to draw circle
+                    l = (np.mean(j, dtype = np.int), 5 + i * 10)
+                    cv.circle(warped, l, 2, (214, 57, 17), 2)
+                elif (np.mean(j, dtype=np.int) >= right_thres):
+                    right = [np.mean(j, dtype=np.int), 5 + i * 10]
+                    dataRight.append(right)
+                    # Tuple to draw circle
+                    r = (np.mean(j, dtype = np.int), 5 + i * 10)
+                    cv.circle(warped, r, 2, (0, 84, 163), 2)
+                # else:
+                #     dataMid.append((np.mean(j, dtype=np.int), 5 + i * 10))
+        except IndexError:
+            pass
+        cv.imshow("warped", warped)
+    return [np.asarray(dataLeft, dtype=np.int), np.asarray(dataRight, dtype=np.int)]
+
+# Using Ransac to fit lane
+def ransacSklearn(data):
+    data = np.sort(data, axis = 0)
+    X = np.asarray(data[: , 0], dtype = np.int32)
+    y = data[: , 1]
+
+    lineX = X.reshape((len(data), 1))
+    ransac = RANSACRegressor(PolynomialRegression(degree = 2), residual_threshold= 2 * np.std(y), random_state=0)
+    ransac.fit(lineX, y)
+    lineY = ransac.predict(lineX)
+    poly_Coef = np.polyfit(X, lineY, 2)
+    lineY = np.polyval(poly_Coef, lineX).reshape((-1, 1))
+    result = np.hstack((lineX, lineY))
+    return np.int32(result)
+
+class PolynomialRegression(object):
+    def __init__(self, degree = 3, coeffs = None):
+        self.degree = degree
+        self.coeffs = coeffs
+    
+    def fit(self, X, y):
+        self.coeffs = np.polyfit(X.ravel(), y, self.degree)
+
+    def get_params(self, deep = False):
+        return {'coeffs': self.coeffs}
+    
+    def set_params(self, coeffs = None, random_state = None):
+        self.coeffs = coeffs
+    
+    def predict(self, X):
+        poly_eqn = np.poly1d(self.coeffs)
+        y_hat = poly_eqn(X.ravel())
+        return y_hat
+    
+    def score(self, X, y):
+        return mean_squared_error(y, self.predict(X))
+
+def get_angle(xLeft, xRight, y):
+    xMid = (xLeft + xRight) // 2
+    error_angle = math.atan((xMid - 160) // 2)
+    return error_angle  
+
+def line_detect(img):
+    process = downsample(img)
+    left, right = getmeanX(process, img)
+    try:
+        leftCurve = ransacSklearn(left)
+        cv.polylines(img, [leftCurve], False, (255, 0, 0), 2)
+    except IndexError:
+        pass
+    try:
+        rightCurve = ransacSklearn(right)
+        cv.polylines(img, [rightCurve], False, (255, 0, 0), 2)
+    except IndexError:
+        pass
+    try:
+        error_angle = get_angle(leftCurve[220], rightCurve[220], 220)
+    except IndexError:
+        error_angle = 0
+    return error_angle
